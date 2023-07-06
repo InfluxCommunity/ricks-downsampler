93516e9aec617a550ef9b847e30882f4ed5f5792
Rick Spencer <rick@influxdata.com>
2023-07-06 18:45:39 -0400

    have a job scheduled for every 1 minute running

---
+++ dockerfile
@@ -0,0 +1,16 @@
+# Use the latest Python runtime as a parent image
+FROM python:latest
+
+# Set the working directory in the container to /app
+WORKDIR /
+
+ADD ./requirements.txt /requirements.txt
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Add the current directory contents into the container at /app
+ADD ./main.py /main.py
+
+# Run the app using Gunicorn
+CMD ["python", "/main.py"]
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("downsample")
+import os
+from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from datetime import datetime
+
+def schedule():
+    scheduler = BlockingScheduler()
+    scheduler.add_job(run, IntervalTrigger(minutes=1), id='my_job')
+    scheduler.start()
+
+def run():
+    now = datetime.now()
+    timestamp = now.strftime('%Y-%m-%d %H:%M:00')
+    print(timestamp)
+
+
+
+if __name__ == "__main__":
+    schedule()
