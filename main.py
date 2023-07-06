37b7e4232eed6affe8130392b19d1f28c2d7624a
Rick Spencer <rick@influxdata.com>
2023-07-06 19:00:11 -0400

    added an envar for run interval minutes

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
 from apscheduler.schedulers.background import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def schedule():
+    interval = int(os.getenv('RUN_INTERVAL'))
     scheduler = BlockingScheduler()
-    scheduler.add_job(run, IntervalTrigger(minutes=1), id='my_job')
+    scheduler.add_job(run, IntervalTrigger(minutes=interval), id='my_job', interval=interval)
+   
     scheduler.start()
 
-def run():
+def run(interval=0):
     now = datetime.now()
-    timestamp = now.strftime('%Y-%m-%d %H:%M:00')
-    print(timestamp)
+    prev = now - timedelta(minutes=interval)
+    stop = now.strftime('%Y-%m-%d %H:%M:00')
+    start = prev.strftime('%Y-%m-%d %H:%M:00')
+    print(f"sample from {start} to {stop}")
 
 
 
