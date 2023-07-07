d53632587ae8ece5e1ffa2ac75b11ef3c2501dfd
Rick Spencer <rick@influxdata.com>
2023-07-07 08:33:41 -0400

    start pushing around time intervals

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ from apscheduler.triggers.interval import IntervalTrigger
 from datetime import datetime, timedelta
 
 def schedule():
-    interval = int(os.getenv('RUN_INTERVAL'))
-    scheduler = BlockingScheduler()
-    scheduler.add_job(run, IntervalTrigger(minutes=interval), id='my_job', interval=interval)
+    run_interval = int(os.getenv('RUN_INTERVAL', 60))
     
+    scheduler = BlockingScheduler()
+    scheduler.add_job(run, IntervalTrigger(minutes=run_interval), args=[run_interval])
+    run(run_interval)
     scheduler.start()
 
-def run(interval=0):
+def run(run_interval):
     now = datetime.now()
-    prev = now - timedelta(minutes=interval)
+    prev = now - timedelta(minutes=run_interval)
     stop = now.strftime('%Y-%m-%d %H:%M:00')
     start = prev.strftime('%Y-%m-%d %H:%M:00')
     print(f"sample from {start} to {stop}")
-
-
 
 if __name__ == "__main__":
     schedule()
