391761944845a7a30db87dac957b6e725b4436ae
Rick Spencer <rick@influxdata.com>
2023-08-01 08:54:28 -0400

    added write retries, updated readme, added a happy path test

--- test_main.py
+++ test_main.py
@@ -1,8 +1,10 @@
 import unittest
-from main import parse_interval
+from unittest.mock import MagicMock, Mock, patch
+from main import parse_interval, write_downsampled_data
 from influxql_generator import field_is_num, generate_fields_string, generate_group_by_string
 from schedule_calculator import get_next_run_time_minutes, get_next_run_time_hours, get_next_run_time, get_then
 from datetime import datetime
+import traceback
 
 class SQLGeneration(unittest.TestCase):
     def test_field_is_num(self):
@@ -66,6 +68,7 @@ class TestNowANdThen(unittest.TestCase):
         test_time = datetime(2023,7,7,12,0)
         then = get_then(1,"m",test_time)
         self.assertEqual(then, datetime(2023,7,7,11,59))
+
 class TestGetNextRunTime(unittest.TestCase):
     def test_get_with_previous_hour(self):
         test_cases = [{"interval_val":1, "interval_type":"h","run_prev":True,"expected_hour":8,
@@ -91,14 +94,14 @@ class TestGetNextRunTime(unittest.TestCase):
         for tc in test_cases:
             test_time = datetime(2023,7,7, tc["clock_hour"], tc["clock_minute"])
             next_run_time = get_next_run_time_hours(1, now=test_time)
-            self.assert_(next_run_time, datetime(2023, 7, tc["expected_hour"], 0))
+            self.assertTrue(next_run_time, datetime(2023, 7, tc["expected_hour"], 0))
 
     def test_days(self):
         test_cases = [{"clock_day": 7, "expected_day":8}]
         for tc in test_cases:
             test_time = datetime(2023,7,tc["clock_day"], 7)
             next_run_time = get_next_run_time_hours(1, now=test_time)
-            self.assert_(next_run_time, datetime(2023, 7, 8, 0, 0))
+            self.assertTrue(next_run_time, datetime(2023, 7, 8, 0, 0))
 
     def test_minutes_basic(self):
         test_cases = [{"clock_minutes":1, "run_interval":1, "expected_minute":2},
@@ -127,6 +130,49 @@ class TestGetNextRunTime(unittest.TestCase):
         now = datetime(2023, 7, 7, 23, 30) 
         next_run_time = get_next_run_time_minutes(120, now=now)
         self.assertEqual(next_run_time, datetime(2023, 7, 8, 0, 0))
+
+class TestWriteData(unittest.TestCase):
+    def test_write_downsampled_data(self):
+        # create a mock write client
+        mock_target_client = Mock()
+        mock_target_client.write = MagicMock()
+
+        # Create a mock dataframe
+        mock_df = MagicMock()
+        mock_df.columns = []
+        mock_df.shape = (0, 0)
+        mock_df.__getitem__.return_value = False
+        mock_df.drop.return_value = mock_df
+
+        # Create a mock batch
+        mock_batch = Mock()
+        mock_batch.to_pandas.return_value = mock_df
+
+        # Setup the mock batch to return a mock dataframe
+        mock_batch.to_pandas.return_value = mock_df
+
+        # Create a mock reader
+        mock_reader = Mock()
+        mock_reader.read_chunk.side_effect = [
+            # Return a mock batch and buffer on the first call
+            (mock_batch, None),
+            # Raise StopIteration on the second call to end the loop
+            StopIteration()
+        ]
+
+        with patch('main.target_client', new=mock_target_client):
+            success, error, row_count, retries = write_downsampled_data(mock_reader)
+
+        # Verify the result
+        assert success
+        assert row_count == 0
+        assert retries == 0
+        assert error is None
+
+        # Verify the mocks were called as expected
+        mock_reader.read_chunk.assert_called()
+        mock_batch.to_pandas.assert_called()
+        mock_target_client.write.assert_called()
 
 if __name__ == "__main__":
     unittest.main()
